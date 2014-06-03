503caa28d057b8486d1238c4766358e748061ff7
Lari Hotari <lhotari@gopivotal.com>
2014-06-03 11:08:55 +0200

    Add example of injecting Spring bean to Ratpack handler closure

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -1,9 +1,23 @@
 import static ratpack.groovy.Groovy.groovyTemplate
 import static ratpack.groovy.Groovy.ratpack
 import hello.Person
+import javax.sql.DataSource
+import groovy.sql.Sql
 
 ratpack {
   handlers {
+    // test accessing Spring beans (the dataSource bean created by Spring Boot)
+    get ('database') { DataSource dataSource ->
+        def sql
+        try { 
+            sql = new Sql(dataSource)
+            def result=[:] + sql.firstRow("select * from Person")
+            render "first row in Person: $result"
+        } finally {
+            sql?.close()
+        }
+    }
+    // test Gorm access
     get {
     	def p = Person.findByFirstName("Homer")
         if( !p ) {
